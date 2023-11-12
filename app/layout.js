1313dbe589774a4fbca57a36f69290f3bf984f4b
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2023-11-11 20:50:36 -0500

    fix: set url for twitter card

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,10 @@ import '@/sass/global.scss';
 const libre = Libre_Baskerville({ subsets: ['latin'], weight: ['400', '700'] })
 
 const description = 'Formando bailarinas desde 1956 en Lima, Surco. Nuestros 64 años de experiencia fomentando la danza y la música se ven reflejados en el amor que le ponemos a cada una de nuestras clases de ballet clásico, danza contemporánea y clases individuales de canto, violín y piano.';
-const mainImage = '/fondo-inicio-2017.jpg';
 const url = process.env.DEPLOY_URL || 'https://www.britanicadeballet.com/';
 
 export const metadata = {
-  //metadataBase: new URL(url),
+  metadataBase: new URL(url),
   title: 'Academia Británica de Ballet',
   description,
   openGraph: {
@@ -18,13 +17,11 @@ export const metadata = {
     description,
     url,
     siteName: 'Academia Británica de Ballet',
-    //images: mainImage
   },
   twitter: {
     title: 'Academia Británica de Ballet',
     description,
     creator: '@academiadballet',
-    //images: [mainImage],
   },
   icons: {
     icon: ['/ico.png']
