6e6ca62c4ead67694718bb30303e67c3d74ce705
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2025-01-27 17:10:20 -0500

    FEAT: tailwind changes for Navigation component

--- app/containers/Navigation/Navigation.js
+++ app/containers/Navigation/Navigation.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { Header } from "@/components";
 import Image from "next/image";
 import cn from 'classnames';
-import style from "./navigation.module.scss";
 import { useAnimate } from 'framer-motion';
 
 const menuOptions = {
@@ -46,17 +45,20 @@ const Navigation = () => {
     });
   };
 
+  const menuUIStyles = "bg-color3 mt-[30px] absolute w-6 h-[2px] rounded-[10px] transition duration-[0.6s] ease-out";
+
   return (
     <Header>
-      <div className={style.navigation}>
-        <a to="id-home" className={style.navigation__logo} smooth="easeInOutCubic">
-          <Image src='/logo.png' alt="Británica de ballet" width={166} height={339} />
+      <div className="flex items-center justify-between bg-color1 opacity-90 fixed h-[75px] w-screen py-2 z-[998]">
+        <a to="id-home" className="h-full pl-[10vw] cursor-pointer"smooth="easeInOutCubic">
+          <Image className="h-full w-auto" src='/logo.png' alt="Británica de ballet" width={166} height={339} />
         </a>
         <nav role="navigation">
-          <ul ref={scope} className={cn(style.navigation__menu)} onClick={handleOpenMenu}>
+          <ul ref={scope} className="top-0 left-0 fixed w-full bg-color1 overflow-hidden m-0 p-0 pt-[50px] h-full opacity-0 hidden" onClick={handleOpenMenu}>
             { Object.keys(menuOptions).map((option) => (
-              <li key={option}>
+              <li key={option} className='text-center p-2 first:mt-12'>
                 <a
+                  className='cursor-pointer text-[150%] text-color3 font-bold w-full'
                   to={option}
                   offset={-75}
                   smooth="easeInOutCubic"
@@ -66,13 +68,14 @@ const Navigation = () => {
                 </a>
               </li>
             )) }
-            <div className={style.navigation__menu__logo}>
+            <div className="flex justify-center align-middle mh-[200px] mt-10">
               <a
                 to="id-home"
                 smooth="easeInOutCubic"
                 style={{ cursor: "pointer" }}
               >
                 <Image
+                  className='h-20 w-auto'
                   src='/logo.png'
                   alt="Británica de ballet"
                   width={166}
@@ -81,14 +84,14 @@ const Navigation = () => {
               </a>
             </div>
           </ul>
-          <div onClick={handleOpenMenu} className={style.navigation__wrap} />
-          <div className={style.navigation__action}>
-            <a className={style.navigation__action__icon}>
-              <div className={cn(style.menui, style.topMenu, { [style.topAnimate]: menu.topMenu })}
+          <div onClick={handleOpenMenu} className="absolute top-5 h-[50px] w-[50px] opacity-0 z-[999] cursor-pointer" />
+          <div className='top-[30px] pr-[15vw]'>
+            <a>
+              <div className={cn(menuUIStyles, { "top-[13px] rotate-45": menu.topMenu, "top-[5px]": !menu.topMenu })}
               />
-              <div className={cn(style.menui, style.midMenu, { [style.midAnimate]: menu.midMenu })}
+              <div className={cn(menuUIStyles, "top-[13px]", { "opacity-0": menu.midMenu })}
               />
-              <div className={cn(style.menui, style.bottomMenu, { [style.bottomAnimate]: menu.bottomMenu })}
+              <div className={cn(menuUIStyles, { "top-[13px] rotate-[-225deg]": menu.bottomMenu, "top-[21px]": !menu.bottomMenu })}
               />
             </a>
           </div>
