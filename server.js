ba83323d4a75cf1a151fc238873a5a1f0d24e68e
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2025-01-31 13:23:07 -0500

    FEAT: adding http2 module

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const http2 = require('http2');
 
+// Crear una app de Express
 const app = express();
 
 // Habilitar compresión para mejorar el rendimiento
 app.use(compression());
 
-// Servir archivos estáticos desde 'out'
+// Servir archivos estáticos desde la carpeta 'out'
 app.use(express.static(path.join(__dirname, 'out'), { extensions: ['html'] }));
 
 // Manejar rutas desconocidas y redirigirlas a index.html (SPA fallback)
@@ -15,8 +17,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'out', 'index.html'));
 });
 
-// Iniciar el servidor en el puerto de Heroku o en el 3000 localmente
+// Usar HTTP/2 con Heroku
+const server = http2.createServer(app);
+
+// Iniciar servidor HTTP/2
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor estático corriendo en el puerto ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Servidor estático corriendo en el puerto ${PORT} con HTTP/2`);
 });
