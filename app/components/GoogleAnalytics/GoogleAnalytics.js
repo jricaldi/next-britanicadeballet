9dee3e9ae7e6576f545a7a56bb3814ad7607359f
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2025-02-07 22:45:54 -0500

    FEAT: adding new approach to run google analytics

--- app/components/GoogleAnalytics/GoogleAnalytics.js
+++ app/components/GoogleAnalytics/GoogleAnalytics.js
@@ -1,32 +1,11 @@
-import Script from "next/script"
+'use client'
+
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
-const GoogleAnalytics = () => {
-
-  //You can show in the console the NEXT_PUBLIC_GOOGLE_ANALYTICS_ID to confirm
-  console.log('NEXT_PUBLIC_GOOGLE_ANALYTICS_ID', NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
-
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-                  `,
-        }}
-      />
-    </>
-  )
+const Analytics = () => {
+  return <GoogleAnalytics gaId={NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
 }
 
-export default GoogleAnalytics
+export default Analytics
